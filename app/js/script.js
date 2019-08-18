c489b75b2eb3cf8199f723b60672d9fbfaba177b
Zzaaf <zzaafamania@gmail.com>
2019-08-18 23:15:13 +0300

    Primitive Lesson #1

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,289 @@
-let i = 100;
+let response = [
+    {
+    "_id": "5d57c12ec06d325a4387b1e8",
+    "index": 0,
+    "guid": "81017c3e-87c3-4b6e-b207-88f2a76f0282",
+    "isActive": false,
+    "balance": "$1,568.05",
+    "picture": "http://placehold.it/32x32",
+    "age": 38,
+    "eyeColor": "blue",
+    "name": "Conrad Mccall",
+    "gender": "male",
+    "company": "FURNIGEER",
+    "email": "conradmccall@furnigeer.com",
+    "phone": "+1 (853) 475-3006",
+    "address": "200 Commercial Street, Faxon, Kentucky, 7463",
+    "about": "Enim ad magna eu veniam. Cupidatat commodo duis id aliqua ad occaecat. Magna proident deserunt ex non ullamco fugiat Lorem. Ipsum est esse consectetur tempor. Amet laboris dolor consectetur nostrud aute occaecat.\r\n",
+    "registered": "2016-04-02T05:28:18 -03:00",
+    "latitude": -33.380073,
+    "longitude": -32.792783,
+    "tags": [
+        "ut",
+        "qui",
+        "nulla",
+        "aliquip",
+        "excepteur",
+        "duis",
+        "enim"
+    ],
+    "friends": [
+        {
+        "id": 0,
+        "name": "Patrick Sloan"
+        },
+        {
+        "id": 1,
+        "name": "Langley Cohen"
+        },
+        {
+        "id": 2,
+        "name": "Decker Ortega"
+        }
+    ],
+    "greeting": "Hello, Conrad Mccall! You have 2 unread messages.",
+    "favoriteFruit": "apple"
+    },
+    {
+    "_id": "5d57c12ef989f14058054f71",
+    "index": 1,
+    "guid": "8c45faae-b141-45a0-96cf-fc916fb81aee",
+    "isActive": true,
+    "balance": "$1,951.26",
+    "picture": "http://placehold.it/32x32",
+    "age": 40,
+    "eyeColor": "green",
+    "name": "Mendoza Little",
+    "gender": "male",
+    "company": "CALCULA",
+    "email": "mendozalittle@calcula.com",
+    "phone": "+1 (920) 467-3236",
+    "address": "878 Berriman Street, Saddlebrooke, Oklahoma, 9766",
+    "about": "Pariatur culpa ad in nostrud minim. Commodo voluptate adipisicing anim esse in cupidatat. Deserunt enim dolore quis irure irure occaecat ullamco magna excepteur sint voluptate sunt. Enim eu deserunt Lorem ea sunt minim.\r\n",
+    "registered": "2016-10-31T02:18:38 -03:00",
+    "latitude": 40.849771,
+    "longitude": 77.88267,
+    "tags": [
+        "Lorem",
+        "cillum",
+        "eiusmod",
+        "mollit",
+        "officia",
+        "qui",
+        "amet"
+    ],
+    "friends": [
+        {
+        "id": 0,
+        "name": "Moss Giles"
+        },
+        {
+        "id": 1,
+        "name": "Bryan Wilson"
+        },
+        {
+        "id": 2,
+        "name": "Elliott Lynch"
+        }
+    ],
+    "greeting": "Hello, Mendoza Little! You have 2 unread messages.",
+    "favoriteFruit": "banana"
+    },
+    {
+    "_id": "5d57c12e32b429f452c038bb",
+    "index": 2,
+    "guid": "6976ad99-b1bf-40ee-a6da-9cda70384fa9",
+    "isActive": true,
+    "balance": "$2,602.24",
+    "picture": "http://placehold.it/32x32",
+    "age": 32,
+    "eyeColor": "brown",
+    "name": "Harmon Callahan",
+    "gender": "male",
+    "company": "HOUSEDOWN",
+    "email": "harmoncallahan@housedown.com",
+    "phone": "+1 (922) 456-2167",
+    "address": "527 Lynch Street, Rivera, Marshall Islands, 6274",
+    "about": "Velit velit quis deserunt qui. Id id esse in esse in in. Aliquip pariatur sit laborum ut reprehenderit laboris cupidatat tempor in. Exercitation id sunt ipsum elit est qui quis eiusmod laboris labore.\r\n",
+    "registered": "2014-10-22T10:26:48 -04:00",
+    "latitude": -69.345137,
+    "longitude": -20.166993,
+    "tags": [
+        "velit",
+        "irure",
+        "cillum",
+        "dolor",
+        "consequat",
+        "Lorem",
+        "mollit"
+    ],
+    "friends": [
+        {
+        "id": 0,
+        "name": "Bass Howell"
+        },
+        {
+        "id": 1,
+        "name": "Mcgowan Fitzgerald"
+        },
+        {
+        "id": 2,
+        "name": "Dominguez Hatfield"
+        }
+    ],
+    "greeting": "Hello, Harmon Callahan! You have 4 unread messages.",
+    "favoriteFruit": "strawberry"
+    },
+    {
+    "_id": "5d57c12e17777bb8f5debf7b",
+    "index": 3,
+    "guid": "e0c6b536-c06b-4c52-b015-43e330c7c7a0",
+    "isActive": true,
+    "balance": "$2,924.95",
+    "picture": "http://placehold.it/32x32",
+    "age": 28,
+    "eyeColor": "green",
+    "name": "Fran Dodson",
+    "gender": "female",
+    "company": "ZILLIDIUM",
+    "email": "frandodson@zillidium.com",
+    "phone": "+1 (883) 563-2153",
+    "address": "533 Richardson Street, Nile, Nevada, 7212",
+    "about": "Magna voluptate cillum amet ad proident ut consectetur non laborum. Consequat laborum magna do incididunt. Labore ea qui sunt nostrud minim occaecat officia.\r\n",
+    "registered": "2017-06-15T09:29:42 -03:00",
+    "latitude": -37.128466,
+    "longitude": -135.816383,
+    "tags": [
+        "fugiat",
+        "ex",
+        "duis",
+        "anim",
+        "qui",
+        "est",
+        "duis"
+    ],
+    "friends": [
+        {
+        "id": 0,
+        "name": "Malinda Molina"
+        },
+        {
+        "id": 1,
+        "name": "Chavez Hull"
+        },
+        {
+        "id": 2,
+        "name": "Delaney Salazar"
+        }
+    ],
+    "greeting": "Hello, Fran Dodson! You have 9 unread messages.",
+    "favoriteFruit": "apple"
+    },
+    {
+    "_id": "5d57c12ebc734d25d3df8825",
+    "index": 4,
+    "guid": "0e3fef1e-38e1-40f1-bf1b-ac581821f4f4",
+    "isActive": false,
+    "balance": "$1,357.60",
+    "picture": "http://placehold.it/32x32",
+    "age": 26,
+    "eyeColor": "blue",
+    "name": "Mullins Tyler",
+    "gender": "male",
+    "company": "MOREGANIC",
+    "email": "mullinstyler@moreganic.com",
+    "phone": "+1 (855) 483-3296",
+    "address": "516 Wyona Street, Beaverdale, Michigan, 367",
+    "about": "Anim anim dolore sit ipsum. Veniam culpa non ipsum eiusmod dolore id adipisicing cillum exercitation. Sunt sint culpa et Lorem ex ad laborum magna. Esse consequat minim proident elit commodo veniam id tempor qui id duis minim anim. Aute ad mollit dolor incididunt sint. Reprehenderit esse adipisicing proident exercitation. Velit do esse velit excepteur id adipisicing.\r\n",
+    "registered": "2016-05-21T01:31:34 -03:00",
+    "latitude": 14.16988,
+    "longitude": -97.644451,
+    "tags": [
+        "sit",
+        "veniam",
+        "eu",
+        "amet",
+        "aliquip",
+        "non",
+        "labore"
+    ],
+    "friends": [
+        {
+        "id": 0,
+        "name": "Jennings Beach"
+        },
+        {
+        "id": 1,
+        "name": "Elvira Turner"
+        },
+        {
+        "id": 2,
+        "name": "Copeland Guy"
+        }
+    ],
+    "greeting": "Hello, Mullins Tyler! You have 7 unread messages.",
+    "favoriteFruit": "apple"
+    },
+    {
+    "_id": "5d57c12e85367b0cc0d9e704",
+    "index": 5,
+    "guid": "ff298014-bc46-471c-89c7-6402a562a679",
+    "isActive": false,
+    "balance": "$2,378.99",
+    "picture": "http://placehold.it/32x32",
+    "age": 30,
+    "eyeColor": "green",
+    "name": "Leann Mckee",
+    "gender": "female",
+    "company": "OPPORTECH",
+    "email": "leannmckee@opportech.com",
+    "phone": "+1 (943) 536-2286",
+    "address": "251 Schenck Court, Teasdale, Alabama, 3986",
+    "about": "Ea culpa ea id est labore. Duis sint culpa veniam enim velit enim consectetur excepteur ea dolore velit. Eiusmod reprehenderit proident ut qui ipsum excepteur nisi cupidatat. Voluptate cillum reprehenderit do amet dolor magna cupidatat occaecat ullamco proident deserunt.\r\n",
+    "registered": "2014-09-06T11:41:33 -04:00",
+    "latitude": 10.808954,
+    "longitude": -113.855057,
+    "tags": [
+        "culpa",
+        "laboris",
+        "et",
+        "ullamco",
+        "cillum",
+        "dolore",
+        "Lorem"
+    ],
+    "friends": [
+        {
+        "id": 0,
+        "name": "Shelby Good"
+        },
+        {
+        "id": 1,
+        "name": "Massey Ross"
+        },
+        {
+        "id": 2,
+        "name": "Juarez Hays"
+        }
+    ],
+    "greeting": "Hello, Leann Mckee! You have 7 unread messages.",
+    "favoriteFruit": "strawberry"
+    }
+];
 
-let b = 100;
+let arr_test = [];
 
-const z = 15;
+response.forEach(element => {
+    arr_test.push(element.name);
+});
+
+let body = document.getElementsByTagName('body')[0];
+let ul = document.createElement('ul');
+body.appendChild(ul);
+
+arr_test.forEach(element => {
+    let li = document.createElement('li');
+    li.innerText = element;
+    ul.appendChild(li);
+});
+console.log(arr_test);
